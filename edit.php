896a50e069df043c68a84b9b21c441792c214c87
Daniel Sutcliffe <dansut@users.sourceforge.net>
2009-11-11 15:19:45 +0000

    Move getFields() call so failed save gets good field vals in subsequent form

--- edit.php
+++ edit.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_person/edit.php,v 1.1 2009/09/23 15:20:22 spiderr Exp $
+// $Header: /cvsroot/bitweaver/_bit_person/edit.php,v 1.2 2009/11/11 15:19:45 dansut Exp $
 
 // Initialization
 require_once('../bit_setup_inc.php');
@@ -19,9 +19,6 @@ if($gContent->isValid()){
 // Set up access to edit services needed by Person
 $gContent->invokeServices('content_edit_function');
 
-// Load field attributes
-$gBitSmarty->assign_by_ref('fields', $gContent->getFields());
-
 // Check if the page has changed
 if(!empty($_REQUEST["save_person"])) {
 	// Check if all Request values are delivered, and if not, set them to avoid error messages.
@@ -32,6 +29,9 @@ if(!empty($_REQUEST["save_person"])) {
 		$gBitSmarty->assign_by_ref('errors', $gContent->mErrors);
 	}
 }
+
+// Load field attributes
+$gBitSmarty->assign_by_ref('fields', $gContent->getFields());
 
 // Display the template
 $gBitSystem->display('bitpackage:person/edit.tpl', tra('Person') , array('display_mode' => 'edit'));
